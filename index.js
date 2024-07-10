8d7bd8d2e227e2cc8d05fc2e1332107c3ea3cf64
Bajana <rilunga323@gmail.com>
2024-07-10 23:28:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const myMessage = document.querySelector('.myMessage');
+console.log(myMessage);
+const theMessageButton = document.querySelector('.theMessageButton');
+theMessageButton.addEventListener('click', function() {
+    alert("button clicked!")
+});
